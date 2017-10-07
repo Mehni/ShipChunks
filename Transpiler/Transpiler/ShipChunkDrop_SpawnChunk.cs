0a615b394d48febec26ae10269ccf3b877be03d0
Mehni <Mehni@users.noreply.github.com>
2017-10-07 13:14:20 +0200

    Initial commit

---
+++ Transpiler/Transpiler/ShipChunkDrop_SpawnChunk.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Harmony;
+using System.Reflection.Emit;
+using System.Reflection;
+
+namespace ShipChunkDrop_Transpiler
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("mehni.rimworld.shipchunks.main");
+
+            harmony.Patch(AccessTools.Method(typeof(IncidentWorker_ShipChunkDrop), "SpawnChunk"), null, null,
+                new HarmonyMethod(typeof(HarmonyPatches), nameof(IncidentWorker_ShipChunkDrop_SpawnChunk_Transpiler)));
+        }
+
+        public static IEnumerable<CodeInstruction> IncidentWorker_ShipChunkDrop_SpawnChunk_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo chunkSelector = AccessTools.Method(typeof(HarmonyPatches), nameof(SelectChunkFromAvailableOptions));
+
+            List<CodeInstruction> instructionList = instructions.ToList();
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldsfld)
+                {
+                    instruction.opcode = OpCodes.Call;
+                    instruction.operand = chunkSelector;
+                }
+                yield return instruction;
+            }
+        }
+
+        public static ThingDef SelectChunkFromAvailableOptions()
+        {
+            IEnumerable<ThingDef> chunk = from defs in DefDatabase<ThingDef>.AllDefs
+                                            where defs.defName.StartsWith("ShipChunk")
+                                            select defs;
+
+            return chunk.RandomElement();
+        }
+    }
+}
